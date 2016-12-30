cc8ad111a95ae738c7d8126a5b43e81fa1305dde
Kody Lawton <Klawton1@Kodys-MacBook-Pro.local>
2016-12-30 18:34:31 -0500

    added create cards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+/*
 if ((cardOne == cardTwo) && (cardThree == cardFour)) {
 	console.log("Got working pairs!");
 };
 if (cardFour != cardOne){
 	alert('These do not match!');
 };
+*/
+var createCards = function(){
+	var board = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++) {
+		card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+}
+createCards();
