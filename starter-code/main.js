d98c950376e095057d81cf8fbe37a4c55c324177
Kody Lawton <Klawton1@Kodys-MacBook-Pro.local>
2017-01-02 21:31:36 -0500

    added shuffle

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,32 +7,30 @@ if (cardFour != cardOne){
 	alert('These do not match!');
 };
 */
-var cards = ['queen', 'king', 'queen', 'king'];
+var cards = ['queen', 'queen', 'king', 'king'];
 
 var createCards = function(){
 	var board = document.getElementById('game-board');
 	for (var i = 0; i < cards.length; i++) {
-		card = document.createElement('div');
+		var card = document.createElement('div');
 		card.className = 'card';
+		card.setAttribute('data-card', cards[i]);
+		card.setAttribute('id', i);
+		card.addEventListener('click', isTwoCards);
 		board.appendChild(card);
 	}
 	return board;
+}
+function shuffleBoard(){
+	board = document.getElementById('game-board');
+	board.innerHTML = "";
+	createCards();
 }
 
 createCards();
 
 var cardsInPlay = [];
 
-for (var i=0; i<cards.length; i++){
-	var card = document.getElementsByClassName("card")[i];
-	card.setAttribute('data-card', cards[i]);
-	card.setAttribute('id', i);
-}
-
-for (var i=0; i<cards.length; i++) {
-	var card = document.getElementsByClassName("card")[i];
-	card.addEventListener('click', isTwoCards);
-}
 
 function isTwoCards(){
 	cardType = this.getAttribute('data-card');
@@ -47,37 +45,77 @@ function isTwoCards(){
 	}
 	if (cardsInPlay.length === 4){
 		console.log("2 cards");
-		var different = isMatch(cardsInPlay);
-		if (different) {
-			cardsInPlay = [];
-		}
+		isMatch(cardsInPlay);
+	}
+	if (cardsInPlay.length === 8){
+		console.log("4 cards");
+		isMatch(cardsInPlay);
 	}
 }
 function isMatch(pair){
-	if (pair[1] === pair[3]){
-		alert("pick a different card!");
-		cardsInPlay.pop();
-		cardsInPlay.pop();
-		return false;
-	}
-	else if (pair[0] === pair[2]){
-		alert("it's a match!");
-		for(var i=0; i<cards.length; i++) {
-			var card = document.getElementsByClassName("card")[i];
-			card.innerHTML = "";
+	if(pair.length === 4){
+		if (pair[1] === pair[3]){
+			alert("pick a different card!");
+			cardsInPlay.pop();
+			cardsInPlay.pop();
 		}
-		return true;
-	}
-	else{
-		alert("not a match!");
-		for(var i=0; i<cards.length; i++) {
-			var card = document.getElementsByClassName("card")[i];
-			card.innerHTML = "";
+		else if (pair[0] === pair[2]){
+			for(var i=0; i<cards.length; i++) {
+				var card = document.getElementsByClassName("card")[i];
+			}
 		}
-		return true;
+		else{
+			alert("not a match!");
+			for(var i=0; i<cards.length; i++) {
+				var card = document.getElementsByClassName("card")[i];
+				card.innerHTML = "";
+			}
+			shuffle(cards);
+			shuffleBoard();
+			cardsInPlay = [];
+		}
 	}
-
+	if(pair.length === 8){
+		if (pair[5] === pair[7]){
+			alert("pick a different card!");
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		}
+		else if (pair[4] === pair[6]){
+			alert("you win!");
+			for(var i=0; i<cards.length; i++) {
+				var card = document.getElementsByClassName("card")[i];
+				card.innerHTML = "";
+			}
+			shuffle(cards);
+			shuffleBoard();
+			cardsInPlay = [];
+		}
+		else{
+			alert("not a match!");
+			for(var i=0; i<cards.length; i++) {
+				var card = document.getElementsByClassName("card")[i];
+				card.innerHTML = "";
+			}
+			shuffle(cards);
+			shuffleBoard();
+			cardsInPlay = [];
+			console.log(cards)
+		}
+	}
 }
+function shuffle(cardList){
+	var newCards = [];
+	length = cardList.length;
+	for (var i = 0; i < length; i++) {
+		index = (i+1) % 4;
+		console.log(index);
+		newCards[i] = cardList[index];
+	}
+	cards = newCards;
+	console.log(cards);
+}
+
 
 
 
